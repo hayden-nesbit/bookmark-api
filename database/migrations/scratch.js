a2999d860ecbf7b1d005a6a9ea86706aeca2a1ea
Hayden Nesbit <hayden.nesbit@campusoutreach.org>
2020-05-22 09:54:08 -0400

    final demo day ready

---
+++ database/migrations/scratch.js
@@ -0,0 +1,21 @@
+
+
+// export GOOGLE_APPLICATION_CREDENTIALS="/Users/haydennesbit/sites/Bookmark-449ad621cdab.json"
+
+
+
+
+// gcloud sql databases create laravel --instance=bookmark-project
+
+
+
+
+// gcloud sql instances describe bookmark-project | grep connectionName
+
+
+// ./cloud_sql_proxy -instances=gifted-chimera-277819:us-central1:bookmark-project=tcp:3306
+// ./cloud_sql_proxy -instances=gifted-chimera-277819:us-central1:bookmark-project=tcp:3306
+
+
+// //instance
+// gifted-chimera-277819:us-central1:bookmark-project
